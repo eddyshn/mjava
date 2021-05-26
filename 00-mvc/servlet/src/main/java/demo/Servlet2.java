61d545926641a72126abd05168c35a2032e13295
eddyshn <eddy.shan@outlook.com>
2021-05-26 08:09:49 +0800

    servlet2

---
+++ 00-mvc/servlet/src/main/java/demo/Servlet2.java
@@ -0,0 +1,22 @@
+package demo;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/servlet2")
+public class Servlet2 extends GenericServlet {
+
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("我是草原上的公牛");
+        System.out.println("我让小牛把我对母牛的爱传给她");
+        System.out.println("该说的话我给小牛说了，小牛听到了给我回复收到，并把我说的话传给母牛");
+        ServletContext application = this.getServletContext();
+        application.setAttribute("name","母牛");
+        application.getRequestDispatcher("/servlet3").forward(servletRequest,servletResponse);
+    }
+}
--- 00-mvc/servlet/src/main/java/demo/ServletDemo.java
+++ 00-mvc/servlet/src/main/java/demo/ServletDemo.java
@@ -6,23 +6,31 @@ import java.io.IOException;
 
 @WebServlet("/servlet")
 public class ServletDemo implements Servlet {
+
+    public ServletDemo() {
+        System.out.println("*****************ServletDemo****************************");
+    }
+
     public void init(ServletConfig servletConfig) throws ServletException {
         System.out.println("*****************init****************************");
     }
 
     public ServletConfig getServletConfig() {
+        System.out.println("*****************service****************************");
         return null;
     }
 
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-
+        System.out.println("*****************service****************************");
     }
 
     public String getServletInfo() {
+
+        System.out.println("*****************getServletInfo****************************");
         return null;
     }
 
     public void destroy() {
-
+        System.out.println("*****************destroys****************************");
     }
 }
